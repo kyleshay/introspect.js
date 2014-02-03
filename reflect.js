825190a651c1d5ea2e55dd06d3259dd256b95fcf
kyle shay <kshay@brandnetworksinc.com>
2014-02-03 15:26:32 -0500

    /*jslint browser: true, evil: true, nomen: true */

--- reflect.js
+++ reflect.js
@@ -1,35 +1,29 @@
-var Reflect = function(file) {
-	function inject(file) {
-		file = file.substring(file.indexOf('function'), file.lastIndexOf('}'));
+var Reflect = function (file) {
+        'use strict';
+        function inject(file) {
+            file = file.substring(file.indexOf('function'), file.lastIndexOf('}'));
+            var privs = file.match(/((var )?\s*?(\w\.*?)=\s*?)?function(\s*?(\w\.*?))?\(/g), // get funcs
+                replace = "return Public;", //this should be passed in as a param
+                instance = eval("new (" + file.replace(replace, null
+                    + "Public._initPrivs = function(funcs){"
+                    + "Public._privates = {};" //init _privates property
+                    + "for(var i=funcs.length;i--;){" //loop the funcs found from `privs`
+                    + "var fn=funcs[i].replace(/function|=|\\s.*?|\\(|var /g,'');" //remove tokens for name
+                    + "if(fn.length == 0) continue;" //skip the empty string funcs
+                    + "try{Public._privates[fn] = eval(fn);}catch(e){" // test to see if func is defined
+                    + "if(e.name=='ReferenceError'){continue;}else{throw e;}}}}\n" //if not, ignore
+                    + replace) //add the thing we replaced back in. a better option would be use .slice
+                    + "})()"); //eval(new(function(){})()) turn it into a func we can call
+            instance._initPrivs(privs); //call the initPrivs function to load the _privates property
+            delete instance._initPrivs; //delete the initiation function
+            return instance;
+        }
 
-		//parse out the function signature
-		var privs = file.match(/((var )?\s*?(\w.*?)=\s*?)?function(\s*?(\w.*?))?\(/g),
-			replace = "return Public;", //this should be passed in as a param
-			instance = eval("new (" + file.replace(replace, ''
-			+ "Public._initPrivs = function(funcs){"
-			+ "Public._privates = {};" //init _privates property
-			+ "for(var i=funcs.length;i--;){" //loop the funcs found from `privs`
-			+ "var fn=funcs[i].replace(/function|=|\\s.*?|\\(|var /g,'');" //remove tokens for name
-			+ "if(fn.length == 0) continue;" //skip the empty string funcs
-			+ "try{Public._privates[fn] = eval(fn);}catch(e){" // test to see if func is defined
-			+ "if(e.name=='ReferenceError'){continue;}else{throw e;}}}}\n" //if not, ignore
-			+ replace) //add the thing we replaced back in. a better option would be use .slice
-			+ "})()"); //eval(new(function(){})()) turn it into a func we can call
-		
-		//call the initPrivs function to load the _privates property
-		instance._initPrivs(privs);
+        var request = new XMLHttpRequest();
+        request.open('GET', file, true);
+        request.send();
 
-		//delete the initiation function
-		delete instance._initPrivs;
-
-		return instance;
-	}
-
-	var request = new XMLHttpRequest
-	request.open('GET', file, true)
-	request.send()
-
-	request.onload = function() {
-		Reflect.injected = inject(this.response);
-	};
-};
+        request.onload = function () {
+            Reflect.injected = inject(this.response);
+        };
+    };
